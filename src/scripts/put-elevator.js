c6e1ff1e15794876c7e0bfd73b05c42cf9129b7e
Daniel Larsson <eadaniel.larsson@gmail.com>
2024-02-15 15:27:44 +0200

    testing abstraction of final route handler to call closest idle elevator

--- src/scripts/put-elevator.js
+++ src/scripts/put-elevator.js
@@ -2,7 +2,6 @@
 import { elevators } from "../../app.js";
 
 
-
 export async function updateElevatorStatus(elevators, req, res) {
   const elevator = elevators.find(e => e.id === parseInt(req.params.id));
   
@@ -14,103 +13,165 @@ export async function updateElevatorStatus(elevators, req, res) {
     return res.json({ message: 'Elevator already at that floor' });
   }
   
-  locatedElevator.destinationFloor = req.body.destinationFloor;
+  elevator.destinationFloor = req.body.destinationFloor;
   
-  const direction = (locatedElevator.currentFloor < req.body.destinationFloor ? 'moving_up' : 'moving_down'); 
-  locatedElevator.status = direction;
+  const direction = (elevator.currentFloor < req.body.destinationFloor ? 'moving_up' : 'moving_down'); 
+  elevator.status = direction;
   
   setTimeout(() => {
-    locatedElevator.status = 'idle';
-    locatedElevator.currentFloor = req.body.destinationFloor;
-    locatedElevator.destinationFloor = 0;
-    return res.json({ message: `Elevator no ${locatedElevator.id} has arrived at floor ${locatedElevator.currentFloor}`});
+    elevator.status = 'idle';
+    elevator.currentFloor = req.body.destinationFloor;
+    elevator.destinationFloor = 0;
+    return res.json({ message: `Elevator no ${elevator.id} has arrived at floor ${elevator.currentFloor}`});
   }, 3000);
 }
+
+
+
+
+
+
+export const pendingCallsQueue = [];
+
+export async function processPendingCalls(elevator) {
+  const nextPendingCall = pendingCallsQueue.shift();
+  
+  if (nextPendingCall) {
+    elevator.destinationFloor = nextPendingCall.floor;
+    const direction = (elevator.currentFloor < nextPendingCall.floor) ? 'moving_up' : 'moving_down';
+    elevator.status = direction;
+    
+    setTimeout(() => {
+      elevator.currentFloor = nextPendingCall.floor;
+      elevator.status = 'idle';
+      console.log(`Elevator ${elevator.id} arrived at floor ${elevator.currentFloor}`);
+      
+      // Recursively process more pending calls if any
+      processPendingCalls(elevator);
+    }, 15000);
+  }
+}
+
+export async function callElevatorToFloor(req, res) {
+  
+  const idleElevators = elevators.filter(elevator => elevator.status === 'idle')
+  
+  let minDifference = Math.abs(elevators[0].currentFloor - parseInt(req.params.floor));
+  let selectedElevator = elevators[0];
+  
+  if (idleElevators.length !== 0) {
+    for (let elevator of elevators) {
+      let currentDifference = Math.abs(elevator.currentFloor - parseInt(req.params.floor));
+      
+      if (currentDifference < minDifference) {
+        minDifference = currentDifference;
+        selectedElevator = elevator;
+      }
+    }
+
+    if (selectedElevator.currentFloor === parseInt(req.params.floor)) {
+      return res.json({ message: 'Elevator already at that floor' });
+    }
+
+    selectedElevator.destinationFloor = parseInt(req.params.floor);
+    
+    const direction = (selectedElevator.currentFloor < parseInt(req.params.floor) ? 'moving_up' : 'moving_down');
+    selectedElevator.status = direction;
+    
+  } else { 
+    for (let elevator of elevators) {
+      let currentDifference = Math.abs(elevator.destinationFloor - parseInt(req.params.floor));
+      if (currentDifference < minDifference) {
+        minDifference = currentDifference;
+        selectedElevator = elevator;
+      }
+    }
+    console.log('execution has passed through here 1');
+    
+    if (selectedElevator.destinationFloor === parseInt(req.params.floor)) {
+      return res.json({ message: 'Elevator arriving at that floor shortly' });
+    }
+    console.log('execution has passed through here 2');
+    
+    // selectedElevator.destinationFloor = parseInt(req.params.floor);
+    
+    const pendingCall = {
+      floor: parseInt(req.params.floor),
+      timestamp: Date.now(),
+    };
+    pendingCallsQueue.push(pendingCall);
+    return res.json({ message: 'Elevator assigned. Please wait for the next available idle elevator.' });
+    // }
+  }
+  
+  // const direction = (selectedElevator.currentFloor < parseInt(req.params.floor) ? 'moving_up' : 'moving_down');
+  // selectedElevator.status = direction;
+  
+  setTimeout(() => {
+    selectedElevator.currentFloor = parseInt(req.params.floor);
+    selectedElevator.status = 'idle';
+    selectedElevator.destinationFloor = 0;
+    
+    processPendingCalls(selectedElevator);
+    
+    res.json({ message: `Elevator ${selectedElevator.id} arrived at floor ${selectedElevator.currentFloor}` });
+  }, 15000);
+
+  // let minDifference = Math.abs(elevators[0].currentFloor - parseInt(req.params.floor));
+  // let selectedElevator = elevators[0];
+  
+  // for (let elevator of elevators) {
+  //   let currentDifference = Math.abs(elevator.currentFloor - parseInt(req.params.floor));
+    
+  //   if (currentDifference < minDifference) {
+  //     minDifference = currentDifference;
+  //     selectedElevator = elevator;
+  //   }
+  // }
+  
+  // if (selectedElevator.currentFloor === req.params.floor) {
+  //   return res.json({ message: 'Elevator already at that floor' });
+  // }
+  
+  // if (selectedElevator.status === 'idle') {
+  //   selectedElevator.destinationFloor = req.params.floor;
+  // } else {
+  //   const pendingCall = {
+  //     floor: req.params.floor,
+  //     timestamp: Date.now(),
+  //   };
+  //   pendingCallsQueue.push(pendingCall);
+  //   return res.json({ message: 'Elevator assigned. Please wait for the next available idle elevator.' });
+  // }
+}
+
+
 
 export const putRoutes = [
   {
     path: '/api/elevators/set-floor/:id',
     handler: async (req, res) => {
-      try{
+      try {
         await updateElevatorStatus(elevators, req, res);
       }
       catch(error) {
         console.error('Error', error.message);
       }
     }
+  },
+  {
+    path: '/api/elevators/call-elevator-to/:floor',
+    handler: async (req, res) => {
+      try {
+        await callElevatorToFloor(req, res);
+      } 
+      catch (error) {
+        console.error('Error', error.message);
+      }
+    }    
   }
 ];
 
 
-// app.put('/api/elevators/call-elevator-to/:floor', callElevatorToFloor);
 
-// export const pendingCallsQueue = [];
-
-// export async function callElevatorToFloor(req, res) {
-//   try {
-//     let minDifference = Math.abs(elevators[0].currentFloor - parseInt(req.params.floor));
-//     let selectedElevator = elevators[0];
-
-//     for (let elevator of elevators) {
-//       let currentDifference = Math.abs(elevator.currentFloor - parseInt(req.params.floor));
-
-//       if (currentDifference < minDifference) {
-//           minDifference = currentDifference;
-//           selectedElevator = elevator;
-//       }
-//     }
-
-//     if (selectedElevator.currentFloor === req.params.floor) {
-//       return res.json({ message: 'Elevator already at that floor' });
-//     }
-    
-//     if (selectedElevator.status === 'idle') {
-//       selectedElevator.destinationFloor = req.params.floor;
-//     } else {
-//       // If the elevator is not idle, store the call in the queue
-//       const pendingCall = {
-//         floor: req.params.floor,
-//         timestamp: Date.now(),
-//       };
-//       pendingCallsQueue.push(pendingCall);
-//       return res.json({ message: 'Elevator assigned. Please wait for the next available idle elevator.' });
-//     }
-    
-//     const direction = (selectedElevator.currentFloor < parseInt(req.params.floor) ? 'moving_up' : 'moving_down');
-//     selectedElevator.status = direction;
-    
-//     setTimeout(() => {
-//       selectedElevator.currentFloor = req.params.floor;
-//       selectedElevator.status = 'idle';
-      
-//       // Process pending calls when the elevator becomes idle
-//       processPendingCalls(selectedElevator);
-      
-//       res.json({ message: `Elevator arrived at floor ${selectedElevator.currentFloor}` });
-//     }, 15000);
-//   } catch (error) {
-//     console.error('Error', error.message);
-//   }
-// }
-
-// export async function processPendingCalls(elevator) {
-//   const nextPendingCall = pendingCallsQueue.shift();
-  
-//   if (nextPendingCall) {
-//     elevator.destinationFloor = nextPendingCall.floor;
-//     const direction = (elevator.currentFloor < nextPendingCall.floor) ? 'moving_up' : 'moving_down';
-//     elevator.status = direction;
-    
-//     setTimeout(() => {
-//       elevator.currentFloor = nextPendingCall.floor;
-//       elevator.status = 'idle';
-//       console.log(`Elevator ${elevator.id} arrived at floor ${elevator.currentFloor}`);
-      
-//       // Recursively process more pending calls if any
-//       processPendingCalls(elevator);
-//     }, 15000);
-//   }
-// }
-
-
-export default { updateElevatorStatus, putRoutes };
+export default { updateElevatorStatus, pendingCallsQueue, processPendingCalls, callElevatorToFloor, putRoutes };
